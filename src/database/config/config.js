5d8e5ef073d5f273163e3c131536bf403b3de1e5
Alex Racciatti <racciattialex@gmail.com>
2022-04-04 17:04:51 -0300

    First commit

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "moviesapi",
+    "password": 'alwaysdata2605',
+    "database": "moviesapi_db",
+    "host": "mysql-moviesapi.alwaysdata.net",
+    "dialect": "mysql"
+  },
+  "test": {
+    // "username": "root",
+    // "password": null,
+    // "database": "database_test",
+    // "host": "127.0.0.1",
+    // "dialect": "mysql"
+  },
+  "production": {
+    "username": "moviesapi",
+    "password": 'alwaysdata2605',
+    "database": "moviesapi_db",
+    "host": "mysql-moviesapi.alwaysdata.net",
+    "dialect": "mysql"
+  }
+}
